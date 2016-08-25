94f02b5d6a9102b6b898e44094bc6d1921c61995
DAVID PATIÐO MONTOYA <d.patino12@uniandes.edu.co>
2016-08-25 11:49:06 -0500

    Cambios

--- src/main/webapp/modules/Reservas/Reserva.module.js
+++ src/main/webapp/modules/Reservas/Reserva.module.js
@@ -3,6 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var recursos [BiblioResourse, CityResourse, LibroResource,UsuarioResource, ReservaResource, videoResource];
 
 
