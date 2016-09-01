a65b4880736e539d76197ae9c0d4421c329a88c7
jsprieto10 <js.prieto10@uniandes.edu.co>
2016-08-31 23:33:29 -0500

    Update usuario.ctrl.js

--- src/main/webapp/src/modules/usuarios/usuario.ctrl.js
+++ src/main/webapp/src/modules/usuarios/usuario.ctrl.js
@@ -8,69 +8,64 @@
                 $scope.usuarios = response.data;    
             }, responseError);
 
-            // el controlador recibió un usuarioId ??
-            // revisa los parámetros (ver el :usuarioId en la definición de la ruta)
             if ($stateParams.usuarioId !== null && $stateParams.usuarioId !== undefined) {
                 
-                // toma el id del parámetro
+
                 id = $stateParams.usuarioId;
-                // obtiene el dato del recurso REST
                 $http.get(context + "/" + id)
                     .then(function (response) {
-                        // $http.get es una promesa
-                        // cuando llegue el dato, actualice currentRecord
-                        $scope.currentusuario = response.data;
+                        $scope.currentUsuario = response.data;
                     }, responseError);
 
-            // el controlador no recibió un usuarioId
             } else
             {
-                // el registro actual debe estar vacio
-                $scope.currentusuario = {
-                    id: undefined /*Tipo Long. El valor se asigna en el backend*/,
-                    name: '' /*Tipo String*/,
-                    lastname: '',
+
+                $scope.currentUsuario = {
+                    id: undefined,
+                    name: '',
+                    username: '',
                     password: '',
-                    direction: ''
+                    address: '',
                 };
               
                 $scope.alerts = [];
             }
 
 
-            this.saverecord = function (id) {
-                currentusuario = $scope.currentusuario;
+            this.saveUsuario = function (id) {
+                currentUsuario = $scope.currentUsuario;
                 
-                // si el id es null, es un registro nuevo, entonces lo crea
+                
                 if (id == null) {
 
-                    // ejecuta POST en el recurso REST
-                    return $http.post(context, currentusuario)
+                    return $http.post(context, currentUsuario)
                         .then(function () {
-                            // $http.post es una promesa
-                            // cuando termine bien, cambie de estado
                             $state.go('usuariosList');
                         }, responseError);
                         
-                // si el id no es null, es un registro existente entonces lo actualiza
                 } else {
                     
-                    // ejecuta PUT en el recurso REST
-                    return $http.put(context + "/" + currentusuario.id, currentusuario)
+                    return $http.put(context + "/" + currentUsuario.id, currentUsuario)
                         .then(function () {
-                            // $http.put es una promesa
-                            // cuando termine bien, cambie de estado
                             $state.go('usuariosList');
                         }, responseError);
                 };
             };
+            
+            this.deleteUsuario = function( usuario) {
+                return $http.delete(context + "/" + usuario.usuarioId)
+                    .then(function () {
+                        $state.reload();
+                    }, responseError);
+            };
 
 
-
+            //Alertas
             this.closeAlert = function (index) {
                 $scope.alerts.splice(index, 1);
             };
 
+            // Función showMessage: Recibe el mensaje en String y su tipo con el fin de almacenarlo en el array $scope.alerts.
             function showMessage(msg, type) {
                 var types = ["info", "danger", "warning", "success"];
                 if (types.some(function (rc) {
