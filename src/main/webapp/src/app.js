e0c72cc79804b8c89f81844ed610d81382706f89
sf.munera10 <sf.munera10@ISIS2603-98.sis.virtual.uniandes.edu.co>
2016-09-01 12:23:30 -0500

    usuarios (por arreglar)

--- src/main/webapp/src/app.js
+++ src/main/webapp/src/app.js
@@ -2,7 +2,7 @@
 
     var mod = ng.module("mainApp", [
         "ui.router",
-        //"usuariosModule",
+        "usuariosModule",
         "bibliotecasModule",
         "librosModule",
         "videosModule",
