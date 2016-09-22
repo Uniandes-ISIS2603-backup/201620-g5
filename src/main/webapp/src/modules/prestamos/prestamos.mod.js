b3662d48af0d8e8631d77ecdea6f9654a7b8e39e
sf.munera10 <sf.munera10@ISIS2603-98.sis.virtual.uniandes.edu.co>
2016-09-22 10:18:20 -0500

    Cambios menores...

--- src/main/webapp/src/modules/prestamos/prestamos.mod.js
+++ src/main/webapp/src/modules/prestamos/prestamos.mod.js
@@ -35,30 +35,6 @@
                         templateUrl: basePath + 'prestamosBiblio.create.html'
                     }
                 }
-            }).state('prestamosSolosList', {
-                url: '/prestamos',
-                parent: 'libroEdit',
-                views: {
-                    'libroInstanceView': {
-                        controller: 'prestamosSolosCtrl',
-                        controllerAs: 'ctrl',
-                        templateUrl: basePath + 'prestamosSolos.list.html'
-                    }
-                }
-
-            }).state('prestamoSoloEdit', {
-                url: '/prestamos/edit/:prestamoId',
-                parent: 'libroEdit',
-                param: {
-                    prestamoId: null
-                },
-                views: {
-                    'libroInstanceView': {
-                        controller: 'prestamosSolosCtrl',
-                        controllerAs: 'ctrl',
-                        templateUrl: basePath + 'prestamosSolos.list.html'
-                    }
-                }
             }).state('prestamosBiblioList', {
                 url: '/prestamos',
                 parent: 'bibliotecaPrestar',
