9200e1167da65d3027d93c53474feeb315f6fa05
jsprieto10 <js.prieto10@uniandes.edu.co>
2016-10-11 10:24:41 -0500

    blog

--- biblioteca-back/src/main/java/co/edu/uniandes/g5/bibliotecas/entities/BlogEntity.java
+++ biblioteca-back/src/main/java/co/edu/uniandes/g5/bibliotecas/entities/BlogEntity.java
@@ -5,13 +5,85 @@
  */
 package co.edu.uniandes.g5.bibliotecas.entities;
 
+import java.io.Serializable;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 /**
  *
- * @author s.rojas19
+ * @author js.prieto10
  */
 @Entity
-public class BlogEntity {
+public class BlogEntity extends RecursoEntity implements Serializable{
     
+    private String titulo;
+    private String nombre;
+    private Long idLibro;
+    private String texto;
+    private String nombreAutor;
+    private Long id;
+    
+   @OneToOne(mappedBy = "libro")
+    private LibroEntity libro = new LibroEntity(); 
+    
+    public LibroEntity getLibro()
+    {
+        return libro;
+    }
+    
+    public void setLibro(LibroEntity libro)
+    {
+        this.libro = libro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getIdLibro() {
+        return idLibro;
+    }
+
+    public void setIdLibro(Long idLibro) {
+        this.idLibro = idLibro;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getNombreAutor() {
+        return nombreAutor;
+    }
+
+    public void setNombreAutor(String nombreAutor) {
+        this.nombreAutor = nombreAutor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+
 }
--- biblioteca-back/src/main/java/co/edu/uniandes/g5/bibliotecas/entities/RecursoEntity.java
+++ biblioteca-back/src/main/java/co/edu/uniandes/g5/bibliotecas/entities/RecursoEntity.java
@@ -1,16 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.uniandes.g5.bibliotecas.entities;
 
+import co.edu.uniandes.g5.bibliotecas.entities.BaseEntity;
 import java.io.Serializable;
 
 /**
  *
  * @author sf.munera10
  */
-public abstract class RecursoEntity extends BaseEntity implements Serializable{
-    
+public abstract class RecursoEntity extends BaseEntity implements Serializable
+{
+   
 }
